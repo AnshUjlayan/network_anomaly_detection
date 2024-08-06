849d1b898068eebf6ae5dd8e9f6fd2e27466ff62
pradyumna <pradyumna.209301309@muj.manipal.edu>
2024-08-06 13:12:43 +0530

    feat: dashboard page

---
+++ client/src/pages/dashboard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+const Dashboard: React.FC = () => {
+  return (
+    <Layout title="Test Network">
+      <div>
+        <h1>Test Network</h1>
+        <p>This is a test network page.</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Dashboard;
