637a688ccad221c6e245554951912fe3a1780f5f
pradyumna <pradyumna.209301309@muj.manipal.edu>
2024-08-06 13:34:44 +0530

    feat: piechart

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Layout from "@theme/Layout";
-
+import PieChart from "../components/PieChart/PieChart";
 const Dashboard: React.FC = () => {
   return (
     <Layout title="Test Network">
       <div>
         <h1>Test Network</h1>
         <p>This is a test network page.</p>
+        <div style={{ width: "100%", height: "400px" }}>
+          <PieChart />
+        </div>
       </div>
     </Layout>
   );
